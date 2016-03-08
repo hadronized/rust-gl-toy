c3be582f4b23952166f246eae39fc3c1ef739971
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-08 11:24:16 +0100

    Opening a window.

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,29 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
 fn main() {
+  let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+  let (mut window, events) =
+    glfw.create_window(WIDTH, HEIGHT, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+  window.make_current();
+
+  while !window.should_close() {
+    window.swap_buffers();
+
+    glfw.poll_events();
+    for (_, event) in glfw::flush_messages(&events) {
+      println!("{:?}", event);
+      match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+          window.set_should_close(true)
+        },
+        _ => {}
+      }
+    }
+  }
 }
